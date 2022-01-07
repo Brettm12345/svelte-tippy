5c7befe9267721f28179a51565fe703cc0e7215f
Brett Mandler <brettmandler@gmail.com>
2022-01-07 16:14:47 -0500

    Added createTippy utility

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import 'virtual:windi.css';
+
+	// if you want to enable windi devtools
+	import {browser} from '$app/env';
+	if (browser) import('virtual:windi-devtools');
+	// ...
+</script>
+
+<slot />
