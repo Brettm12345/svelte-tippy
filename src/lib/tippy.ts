6a279dacf8561d6c86de1567a5599c927acb05d5
Brett Mandler <brettmandler@gmail.com>
2022-01-08 15:53:13 -0500

    Updated tsdoc

--- src/lib/tippy.ts
+++ src/lib/tippy.ts
@@ -11,6 +11,20 @@ export interface TippyReturn {
   destroy: () => void;
 }
 export type Tippy = (element: HTMLElement, props?: TippyProps) => TippyReturn;
+/**
+ * Svelte action for rendering a [Tippy.JS](https://atomiks.github.io/tippyjs/) tooltip
+ * 
+ * @example
+ * ```svelte
+ * <script lang="ts">
+ *   import {tippy} from '$lib/tippy';
+ *   import 'tippy.js/dist/tippy.css'; // optional
+ * </script>
+ * <button use:tippy={{content: 'Test'}}>Test</button>
+```
+ * @param element The element to target (omitted with use)
+ * @param props [Tippy.js Props](https://atomiks.github.io/tippyjs/v6/all-props/)
+ */
 export const tippy: Tippy = (element, props) => {
   const {destroy, setProps: update} = runTippy(element, props);
   return {
@@ -35,7 +49,7 @@ export type CreateTippy = (defaultProps: TippyProps) => Tippy;
  * <button use:tippy={{content: 'Test'}}>Test</button>
  * ```
  * ------------------------------------------
- * @param defaultProps The default properties to pass to tippy.js see [tippy.js props](https://atomiks.github.io/tippyjs/v6/all-tops/)
+ * @param defaultProps The default properties to pass to tippy.js see [tippy.js props](https://atomiks.github.io/tippyjs/v6/all-props/)
  * @returns A svelte action for rendering the tippy.js tooltip
  */
 export const createTippy: CreateTippy = defaultProps => (element, props) =>
